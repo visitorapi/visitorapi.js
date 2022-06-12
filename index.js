20c2160f4de49f69b10302a031e75c8c74e3befa
Chaoming Li <email@chaoming.li>
2022-06-13 09:21:07 +1000

    completed the script

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function VisitorAPI(projectId, successHandler, failureHandler){
+const { default: axios } = require("axios");
 
+function VisitorAPI(projectId, successHandler, failureHandler){
+    axios({
+        method: 'get',
+        url: 'https://api.visitorapi.com/api/?pid='+projectId,
+    }).then(function(response){
+        if(typeof(successHandler) === 'function'){
+            successHandler(response);
+        }
+    }).catch(function(error){
+        if(typeof(failureHandler) === 'function'){
+            failureHandler(error);
+        }
+    });
 }
 
 module.exports = VisitorAPI;
